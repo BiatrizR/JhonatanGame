2402ecc39f34833f393ec8119334e08b9c74c6d1
Bia <82560900+BiatrizR@users.noreply.github.com>
2022-06-16 09:41:03 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,35 +7,17 @@ var engine, world;
 var canvas
 var ground
 
-//1. criar plano de fundo
-var bgImg
-var SpaceShipAnimation, SpaceShip
-
-var angle = 90;
-
-
-function preload(){
-bgImg = loadImage("assets/bgSpace.gif");
-
-SpaceShiphipAnimation = loadAnimation("./assets/SpaceShip/s1.png","./assets/SpaceShip/s2.png","./assets/SpaceShip/s3.png",
-"./assets/SpaceShip/s4.png","./assets/SpaceShip/s5.png","./assets/SpaceShip/s6.png","./assets/SpaceShip/s7.png","./assets/SpaceShip/s8.png","./assets/SpaceShip/s9.png",)
 
 
 
-}
+
+
+
+
 function setup() {
   canvas = createCanvas(1200, 600);
   engine = Engine.create();
   world = engine.world;
-
-
-
- 
-  SpaceShip = createSprite(width/2, height/2 + 200, 50,50)
-  SpaceShip.addAnimation("Space", SpaceShiphipAnimation)
-
-  SpaceShip.frameDelay = 5
-
 
 
 
@@ -44,7 +26,7 @@ function setup() {
 function draw(){
 
  
-  image(bgImg, 0,0, width, height);
+background(225)
   
   Engine.update(engine);
  
